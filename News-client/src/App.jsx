93306cb572d837a24a68e55442b94042775c8d54
Wassim-NM <wassimmahmoud200500@gmail.com>
2025-09-01 21:06:41 +0300

    Card form, root about to be ready

--- News-client/src/App.jsx
+++ News-client/src/App.jsx
@@ -1,12 +1,12 @@
 import MyRoutes from './Routes/routes';
 import './Styles/colors.css';
 import './Styles/utilities.css';
-
+import Card from './Components/Card/card.jsx';
 
 function App() {
   return (
     <>
-      
+      <Card/>
     </>
   );
 };
--- News-client/src/Components/Card/card.jsx
+++ News-client/src/Components/Card/card.jsx
@@ -1,13 +1,16 @@
 import './card.css';
 
-const Card = ({image, title, info, category}) => {
+const Card = ({ image, title, info, category }) => {
 
-    return(
+    return (
         <div className="card">
-            <img className="card-image" src={image} alt="photo" />;
-            <span className="card-category">{category}</span>;
-            <h2 className="card-title">{title}</h2>;
-            <p className="card-info">{info}</p>;
+            <img className="card-image" src='../../Assets/wlogo.jpg' alt="photo" />
+            <div className='card-img-cov'>
+                <span className="card-category">{category}</span>
+                <h2 className="card-title">{title}</h2>
+                <h3 className='card-head-light'>text</h3>
+            </div>
+            <p className="card-info">{info}</p>
         </div>
     );
 
@@ -15,8 +18,8 @@ const Card = ({image, title, info, category}) => {
 
 export default Card;
 
-const Rated_card = ({image, title, info, category}) => {
-    return(
+const Rated_card = ({ image, title, info, category }) => {
+    return (
         <div className="rated-card">
             <img className="rated-card-image" src={image} alt="photo" />;
             <span className="rated-card-category">{category}</span>;
@@ -26,3 +29,5 @@ const Rated_card = ({image, title, info, category}) => {
     );
 
 };
+
+export { Rated_card };
