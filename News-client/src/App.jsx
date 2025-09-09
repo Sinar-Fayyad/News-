54d2726fbec2ed3df4f01898a3d8f17015bb4989
Wassim-NM <wassimmahmoud200500@gmail.com>
2025-09-09 14:51:09 +0300

    npm error solving

--- News-client/src/App.jsx
+++ News-client/src/App.jsx
@@ -1,11 +1,15 @@
-import MyRoutes from './Routes/routes';
+import MyRoutes from './Routes/routes.jsx';
 import './Styles/colors.css';
 import './Styles/utilities.css';
+
+import Categories from './Components/Category/category.jsx';
+import Card from './Components/Cards/card.jsx';
 
 function App() {
   return (
     <>
-      
+      <Card/>
+      <MyRoutes/>
     </>
   );
 };
--- News-client/src/Components/Button/button.jsx
+++ News-client/src/Components/Button/button.jsx
@@ -1,4 +1,4 @@
-import styles from './style.module.css'
+import styles from './button.css'
 import { useNavigate } from 'react-router-dom';
 
 const Button = ({ title , className, onClickListener}) => {
@@ -6,6 +6,6 @@ const Button = ({ title , className, onClickListener}) => {
     return (
         <button className={`${className}`} onClick={onClickListener} >{title}</button>
     );
-}
+};
 
 export default Button;
