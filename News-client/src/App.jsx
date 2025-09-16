db5c21715620903a72f36918192178a7d020828a
Sino57 <sinarfayyad5@gmail.com>
2025-09-16 16:32:36 +0300

    Revert "Merge branch 'main' into frontend"
    This reverts commit 2fa9ebc17b7800b5566e5672dabfa127f7845e72, reversing
    changes made to e45a4c0a2cb7e8d4db072d0d2a4318b52551dd9a.

---
+++ News-client/src/App.jsx
@@ -0,0 +1,11 @@
+import MyRoutes from './Routes/routes';
+import './Styles/colors.css'
+import './Styles/utilities.css'
+
+function App() {
+  return (
+    <MyRoutes/>
+  )
+}
+
+export default App
